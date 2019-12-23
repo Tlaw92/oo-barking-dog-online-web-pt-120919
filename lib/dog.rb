432a5459a68954dae44edf12142fcb72bbf769ec
travis Lawson <travislawclimb@gmail.com>
2019-12-23 17:11:15 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+ fido = Dog.new
+fido.name = "Fido"
+ 
+fido.name
+# => "Fido"
+ 
+fido.bark
+woof!
